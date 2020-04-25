67f2341512e4e254971070abbb30a54b56363c81
'Agam Gore' <gore.agam@gmail.com>
2020-04-24 22:22:30 -0400

    Initial Commi

---
+++ waterPlant/src/main/java/com/wplnt/akshay/waterPlant/WaterPlantApplication.java
@@ -0,0 +1,13 @@
+package com.wplnt.akshay.waterPlant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterPlantApplication.class, args);
+	}
+
+}
---
+++ waterPlant/src/test/java/com/wplnt/akshay/waterPlant/WaterPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wplnt.akshay.waterPlant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
